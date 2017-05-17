d8d507ca2da4419e6dda90142d44af8914c62dfd
Steve <alucard263096@126.com>
2017-05-17 23:45:02 +0800

    fx5

--- workspace_copy/development/typescript/providers/api.ts
+++ workspace_copy/development/typescript/providers/api.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { LoadingController, Loading} from 'ionic-angular';
+import { Loading} from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Headers, RequestOptions } from '@angular/http';
+import { RequestOptions } from '@angular/http';
 import { ApiConfig } from '../app/api.config'
 
 import 'rxjs/add/operator/toPromise';
