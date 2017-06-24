b7e3ca3edca02dc2a5edd273993b30a209b82183
Steve <alucard263096@126.com>
2017-06-25 02:07:12 +0800

    fix

--- templates/apps/board_component.js
+++ templates/apps/board_component.js
@@ -11,7 +11,7 @@
     		var id=$(this).attr("pluginid");
     		var url="{{$rootpath}}appcomponents/"+id+"/install.php?app_id={{$appinfo.id}}";
     		getJSON(url,{},function(data){
-    			info("添加成功");
+    			info("添加成功，请手动刷新页面");
     		});
     	});
 
